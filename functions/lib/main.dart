27ae2cb88a279e1cd83342e4c4b4f42fd217b855
i741978 <johannes.kling@sap.com>
2024-04-13 01:24:34 +0200

    asd

--- functions/lib/main.dart
+++ functions/lib/main.dart
@@ -1,15 +1,29 @@
-import 'dart:async';
-import 'package:dart_appwrite/dart_appwrite.dart';
 
 // This is your Appwrite function
 // It's executed each time we get a request
+import 'dart:io';
+import 'dart:js_interop';
+
+import 'package:dart_appwrite/dart_appwrite.dart';
+
+//user, adimin, owner		
+
 Future<dynamic> main(final context) async {
 // Why not try the Appwrite SDK?
   //
-  // final client = Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(Platform.environment['APPWRITE_FUNCTION_PROJECT_ID'])
-  //    .setKey(Platform.environment['APPWRITE_API_KEY']);
+  final client = Client()
+     .setEndpoint('https://cloud.appwrite.io/v1')
+     .setProject(Platform.environment['APPWRITE_FUNCTION_PROJECT_ID'])
+     .setKey(Platform.environment['APPWRITE_API_KEY']);
+
+  final account = Account(client);
+  final user = Users(client);
+
+
+  
+
+  // account.updateName(name: name)
+  user.create(userId: ID.unique(), email: )
 
   // You can log messages to the console
   context.log('Hello, Logs!');
@@ -23,6 +37,8 @@ Future<dynamic> main(final context) async {
     // `res.send()` dispatches a string back to the client
     return context.res.send('Hello, World!');
   }
+
+  context.log(context.req.jsify());
 
   // `res.json()` is a handy helper for sending JSON
   return context.res.json({
