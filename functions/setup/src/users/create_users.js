aabdae9bb1cf9df29cc1ab9e71a6e5925ee43260
i741978 <johannes.kling@sap.com>
2024-04-20 18:57:00 +0200

    sad

--- functions/setup/src/users/create_users.js
+++ functions/setup/src/users/create_users.js
@@ -23,7 +23,7 @@ const createUser = async ({ email, password, name, schoolShorthand, role, log })
     switch (role) {
         case 'm':
             //await users.updateLabels(userId, ['admin', 'manager']);
-            await teams.createMembership(schoolShorthand, ['member', 'guest'], "appwrite.io", email, userId,);
+            await teams.createMembership(schoolShorthand, ['member', 'guest'], "localhost", email, userId,);
             break;
         case 'a':
             // await users.updateLabels(userId, ['admin']);
