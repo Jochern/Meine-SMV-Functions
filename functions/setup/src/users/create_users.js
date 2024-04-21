8cec18cca3c120a731bf51755251b9a45c8c248f
i741978 <johannes.kling@sap.com>
2024-04-21 16:07:50 +0200

    s

--- functions/setup/src/users/create_users.js
+++ functions/setup/src/users/create_users.js
@@ -15,6 +15,7 @@ const createUser = async ({ email, password, name, schoolShorthand, role, log })
     try {
         await users.create(userId, email, password, name || '');
     } catch (error) {
+        print(error)
         throw Error('Could not create User. User already exists.', { cause: error })
     }
 
